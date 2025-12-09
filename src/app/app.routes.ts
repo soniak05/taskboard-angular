b141a9f18f16dd236696d3dd6d0790a75304fef4
Sonia Kendil <sonia@example.com>
2025-12-09 19:39:12 +0100

    nuevo componente about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home';
 import { TasksComponent } from './components/tasks/tasks';
-
+import { AboutComponent } from './components/about/about';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'tasks', component: TasksComponent },
+    { path: 'about', component: AboutComponent },
     { path: '**', redirectTo: '' }
 ];
